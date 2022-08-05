66888a796dbe612085168ea41a26a1bd38b77572
Jibreel Utley <jmu3dx@gmail.com>
2022-08-05 15:04:08 -0400

    moved document text

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import "./styles.css";
-
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+ Display  Object  with  property name using each letter.
+Each propertie's value will display the number of times That propery  was shown in the array
+  Example: {a:1,b3,c5..etc}  then console.log the result
+</div>
+<div>
+</div>
+`;
 var arr = ["a", "b", "s", "d", "a", "c", "a", "b", "s", "r", "b"];
 var letters = {};
 const set = new Set(arr);
@@ -13,13 +21,4 @@ function createObject() {
   console.log(letters);
 }
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
- Display  Object  with  property name using each letter.
-Each propertie's value will display the number of times That propery  was shown in the array
-  Example: {a:1,b3,c5..etc}  then console.log the result
-</div>
-<div>
-</div>
-`;
 createObject();
