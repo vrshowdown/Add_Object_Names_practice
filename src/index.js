701129bb6d39bf50976ace36edca13405fa9174a
Jibreel Utley <jmu3dx@gmail.com>
2022-08-05 14:48:53 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import "./styles.css";
+
+var arr = ["a", "b", "s", "d", "a", "c", "a", "b", "s", "r", "b"];
+var letters = {};
+const set = new Set(arr);
+const newArr = [...set];
+
+function createObject() {
+  for (var i = 0; i < newArr.length; i++) {
+    let z = arr.filter((element) => element === newArr[i]).length;
+    letters[newArr[i]] = z;
+  }
+  console.log(letters);
+}
+
+createObject();
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+ Display  Object  with  property name using each letter.
+Each propertie's value will display the number of times That propery  was shown in the array
+  Example: {a:1,b3,c5..etc}  then console.log the result
+</div>
+<div>
+</div>
+`;
